e54fa9e8f411d253d81735fa3a549747101c271e
Ivan Kosolapov <polzuka@gmail.com>
2021-11-29 12:54:45 +0500

    Imprioved findRecord()

--- dist/findRecord.js
+++ dist/findRecord.js
@@ -1,9 +1,33 @@
-const findRecord = (template, parameter, key, value) => {
-  if (template[parameter] === undefined) {
-    throw new Error(`Unknown parameter '${parameter}'`);
+const {isArray, isObject} = require('lodash');
+
+const findItem = (items, key, value) => {
+  if (!isArray(items)) {
+    throw new Error(`Items is not an array`);
   }
-  const stringValue = String(value);
-  return template[parameter].find((record) => record[key] === stringValue);
+  return items.find((item) => item[key] === value);
+}
+
+const findRecord = (...args) => {
+  if (args.length === 4) {
+    const [template, parameter, key, value] = args;
+
+    if (!isObject(template)) {
+      throw new Error(`Template is not an object`);
+    }
+
+    if (template[parameter] === undefined) {
+      throw new Error(`Unknown parameter '${parameter}'`);
+    }
+
+    return findItem(template[parameter], key, String(value));
+  }
+
+  if (args.length === 3) {
+    const [items, key, value] = args;
+    return findItem(items, key, value);
+  }
+
+  throw new Error(`Incorrect using of findRecord()`);
 }
 
 module.exports = findRecord;
