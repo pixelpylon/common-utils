20befc383fdee1c557f93d53c66e7aafaf2fb1f9
Ivan Kosolapov <polzuka@gmail.com>
2021-12-28 21:05:21 +0500

    Optional call parameters

--- dist/declares/RpcClient.d.ts
+++ dist/declares/RpcClient.d.ts
@@ -1,5 +1,5 @@
 export declare class RpcClient {
     constructor (url: string)
-    call (method: string, params: object): Promise<object>
+    call (method: string, params?: object): Promise<object>
     static new (url: string): RpcClient
 }
