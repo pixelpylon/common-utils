df28c07c79a11bdad71e585f6ce3baab59faa543
Ivan Kosolapov <polzuka@gmail.com>
2022-01-11 17:46:28 +0500

    loadRemoteConfig declaring

--- dist/declares/main.d.ts
+++ dist/declares/main.d.ts
@@ -5,6 +5,7 @@ export {IAxiosApiClient, AxiosApiClient} from "./AxiosApiClient"
 export {RpcServer} from "./RpcServer"
 export {RpcClient} from "./RpcClient"
 export {
+    CONFIG_PROPERTY_NAMES_UNION,
     CONFIG_PROPERTY_TYPES_UNION,
     CONFIG_PROPERTY_TYPES,
     loadRemoteConfig,
