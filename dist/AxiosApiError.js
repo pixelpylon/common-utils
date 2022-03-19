5d280091217b20b39c66a0012552041e752172d6
Ivan Kosolapov <polzuka@gmail.com>
2022-03-19 19:15:24 +0500

    Improved error logging

--- dist/AxiosApiError.js
+++ dist/AxiosApiError.js
@@ -1,9 +1,10 @@
 const {get, upperCase} = require("lodash")
 const ServiceError = require("./ServiceError")
 
-const formatErrorMessage = (method, path, requestData, status, responseData) => {
+const formatErrorMessage = (method, path, requestData, status, responseData, originalMessage) => {
     return [
         `${upperCase(method)} ${path} ${status}`,
+        originalMessage,
         requestData && `REQUEST: ${JSON.stringify(requestData)}`,
         responseData && `RESPONSE: ${JSON.stringify(responseData)}`,
     ].filter(Boolean).join('\n')
@@ -13,11 +14,11 @@ class AxiosApiError extends ServiceError {
     constructor(error) {
         super({})
         const method = get(error, 'config.method', 'UNKNOWN')
-        const path = get(error, 'request.path', 'UNKNOWN')
+        const path = get(error, 'config.url', 'UNKNOWN')
         const requestData = get(error, 'config.data')
         const status = get(error, 'response.status', 'UNKNOWN')
         const responseData = get(error, 'response.data')
-        this.message = formatErrorMessage(method, path, requestData, status, responseData)
+        this.message = formatErrorMessage(method, path, requestData, status, responseData, error.message)
         this.data = responseData
     }
 }
