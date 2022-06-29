6a9cb99bcf7d67f5f3356f109d16a3de5d7e4147
Ivan Kosolapov <polzuka@gmail.com>
2022-06-29 15:35:28 +0500

    dateRange and duration validators

--- dist/declares/parseDateRanges.d.ts
+++ dist/declares/parseDateRanges.d.ts
@@ -5,5 +5,5 @@ export type DateRange = {
   end: Moment
 }
 
-export declare function parseDateRanges(string): DateRange[]
+export declare function parseDateRanges(source: string): DateRange[]
 export declare function isDateInDateRanges(date: string | Date | Moment, dateRanges: string): boolean
