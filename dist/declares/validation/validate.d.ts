0a3ceb739c0eddc631145cc906de71d3b62614ed
Ivan Kosolapov <polzuka@gmail.com>
2023-09-27 16:39:33 +0500

    Added isEqual() validator

--- dist/declares/validation/validate.d.ts
+++ dist/declares/validation/validate.d.ts
@@ -25,6 +25,7 @@ export declare const isBlank: Validator<string>
 export declare const isObject: Validator<Object>
 export declare const isArray: Validator
 export declare const isValid: Validator
+export declare function isEqual(value: any): Validator
 export declare const isDateRanges: Validator<string>
 export declare const isDuration: Validator<string>
 export declare function isIn(list: any[]): Validator
